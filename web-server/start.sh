ce033e1dde02ef024153f7b88f89e72684dcf445
Ferrari <jxf9385@rit.edu>
2018-04-18 02:02:47 -0400

    removed old uneccesary files

--- web-server/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/usr/local/glassfish4/bin/asadmin start-domain
-/usr/local/glassfish4/bin/asadmin -u admin deploy /Skitter_war.war
-/usr/local/glassfish4/bin/asadmin stop-domain
-/usr/local/glassfish4/bin/asadmin start-domain --verbose
