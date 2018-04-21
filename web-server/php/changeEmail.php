1bbbd937f9c1c5712d1d0888ba52850440234656
iAppropriate <lxc6101@rit.edu>
2018-04-21 01:57:29 -0400

    Added auth for email

--- web-server/php/changeEmail.php
+++ web-server/php/changeEmail.php
@@ -1,50 +1,64 @@
 <?php
 
-include_once("common.php");
-
+if(!isset($_COOKIE['session'])){
+	header("Location: /index.html");
 	
-	if(isset($_GET["email"], $_GET["rit_user"])){
-		
-		if(!empty($_GET["email"]) && !empty($_GET["rit_user"])) {
+}else{
 
-			if($stmt = $con->prepare("UPDATE users SET email=? where rit_user=?")){
+	include_once("common.php");
 
-				$email = strip_tags($_GET["email"]);
-				$rit_user = strip_tags($_GET["rit_user"]);
-
-				if($stmt->bind_param("ss",$email, $rit_user)){
-					
-					if(!$stmt->execute()){
-						die("Error - Issue executing prepared statement: " . mysqli_error($con));
-					}
-				}else{
-					die("Error - Issue binding prepared statement: " . mysqli_error($con));
-				}
-
-				if($stmt->affected_rows == 1){
-
-					echo htmlspecialchars("True - Email was updated for $rit_user", ENT_QUOTES, 'UTF-8');
-				}else{
-					echo htmlspecialchars("False - Email was NOT updated for $rit_user", ENT_QUOTES, 'UTF-8');
-				}
-
-				if($stmt->close()){
-
-					
-				}else{
-					die("Error - Failed to close prepared statement" . mysqli_error($con));
-				}
-
-			}else{
-				die("Error - Issue preparing statement: " . mysqli_error($con));
-				}
-		}else{
-			die("One or more required parameters is null");
-			}
-
+	if(!isAuthenticated($_COOKIE['session'])){
+		unset($_COOKIE['session']);
+		setcookie($_COOKIE['session'], '', time()-3600, '/');
+		header("Location: /index.html");
+	
 	}else{
-		die("One of more required parameters is missing");
-		}
+
 	
-	
+		if(isset($_GET["email"])){
+			
+			if(!empty($_GET["email"])) {
+
+				if($stmt = $con->prepare("UPDATE skitter.users INNER JOIN skitter.sessions ON users.rit_user = sessions.username SET users.email = ? WHERE sessions.session_id = ?")){
+
+					$email = strip_tags($_GET["email"]);
+					$session_id = strip_tags($_COOKIE['session']);
+
+					if($stmt->bind_param("ss",$email, $session_id)){
+						
+						if(!$stmt->execute()){
+							die("Error - Issue executing prepared statement: " . mysqli_error($con));
+						}
+					}else{
+						die("Error - Issue binding prepared statement: " . mysqli_error($con));
+					}
+
+					if($stmt->affected_rows == 1){
+
+						echo htmlspecialchars("True - Email was updated", ENT_QUOTES, 'UTF-8');
+					}else{
+						echo htmlspecialchars("False - Email was NOT updated", ENT_QUOTES, 'UTF-8');
+					}
+
+					if($stmt->close()){
+
+						
+					}else{
+						die("Error - Failed to close prepared statement" . mysqli_error($con));
+					}
+
+				}else{
+					die("Error - Issue preparing statement: " . mysqli_error($con));
+					}
+			}else{
+				die("The required parameter is null");
+				}
+
+		}else{
+			die("The required parameter is missing");
+			}
+		
+	}
+}
+
 ?>
